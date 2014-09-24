6a4fa3f181dddc9b4c78a256c8d98ad90d997b36
Ray Villalobos <ray@lynda.com>
2014-09-23 23:00:03 -0400

    ch02_04 added multiple routes

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -6,5 +6,14 @@ myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/login', {
       templateUrl: 'views/login.html'
+    }).
+    when('/register', {
+      templateUrl: 'views/register.html'
+    }).
+    when('/meetings', {
+      templateUrl: 'views/meetings.html'
+    }).
+    otherwise({
+      redirectTo: '/login'
     });
 }]);
