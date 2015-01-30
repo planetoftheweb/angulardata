b1ff4bf7a7034fc18678937cbab427a3d3f2df18
Ray Villalobos <ray@lynda.com>
2015-01-29 20:00:43 -0500

    ch04_03e

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -4,5 +4,12 @@ myApp.controller('MeetingsController',
   var ref = new Firebase('https://attendanceldcapp.firebaseio.com/meetings');
   var meetings = $firebase(ref);
   $scope.meetings = meetings.$asObject();
-
+  $scope.addMeeting = function() {
+    meetings.$push({
+      name: $scope.meetingname,
+      date: Firebase.SeverValue.TIMESTAMP
+    }).then(function() {
+      $scope.meetingname = '';
+    });
+  }; //addmeeting
 }); //MeetingsController
