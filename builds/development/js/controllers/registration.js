aada42cdee25edc80bdd7dfc02095766a5938235
Ray Villalobos <ray@lynda.com>
2015-01-30 00:25:13 -0500

    05_01_User Registration

--- builds/development/js/controllers/registration.js
+++ builds/development/js/controllers/registration.js
@@ -1,9 +1,19 @@
 myApp.controller('RegistrationController', 
-  function($scope, $location) {
+  function($scope, $firebaseAuth, $location) {
+
+  var ref = new Firebase('https://attendanceldcapp.firebaseio.com/');
+  var auth = $firebaseAuth(ref);
   
   $scope.login = function() {
-    $location.path('/meetings');
-  } //login
+    auth.$authWithPassword({
+      email: $scope.user.email,
+      password: $scope.user.password
+    }).then(function(user) {
+      $location.path('/meetings');
+    }).catch(function(error) {
+      $scope.message = error.message;
+    });
+  }; //login
 
   $scope.register = function() {
     $location.path('/meetings');
