79dc80d5bd7006795ccbb8c68f1a96c8d3c80d2e
Ray Villalobos <ray@lynda.com>
2015-01-31 03:09:47 -0500

    ch05_04 Storing Info

--- builds/development/js/services/authentication.js
+++ builds/development/js/services/authentication.js
@@ -18,7 +18,20 @@ myApp.factory('Authentication', function($firebase,
       return auth.$createUser({
         email: user.email,
         password: user.password
-      });
+      }).then(function(regUser) {
+        var ref = new Firebase(FIREBASE_URL+'users');
+        var firebaseUsers = $firebase(ref);
+
+        var userInfo = {
+          date : Firebase.ServerValue.TIMESTAMP,
+          regUser : regUser.uid,
+          firstname: user.firstname,
+          lastname : user.lastname,
+          email: user.email
+        }; //user info
+
+        firebaseUsers.$set(regUser.uid, userInfo);
+      }); //promise
     } //register
 
   }; //myObject
