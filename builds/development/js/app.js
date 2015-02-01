cf2e0904c79f3d6097b5e6d5eeb4c39333cd1483
Ray Villalobos <ray@lynda.com>
2015-02-01 01:50:17 -0500

    ch05_07 added route control

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -5,6 +5,16 @@ var myApp = angular.module('myApp', ['ngRoute',
 var appControllers = angular.module('appControllers',
   ['firebase']);
 
+myApp.run(['$rootScope', '$location', function($rootScope, $location) {
+  $rootScope.$on('$routeChangeError',
+  function(event, next, previous, error) {
+    if(error === 'AUTH_REQUIRED') {
+      $rootScope.message='Sorry, you must log in to access that page';
+      $location.path('/login');
+    }
+  });
+}]);
+
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
     when('/login', {
