b57dac0f09ab20eb5be6c4ec58e1cb0c97d89efc
Ray Villalobos <ray@lynda.com>
2015-02-02 00:42:14 -0500

    ch06_05 checkin working

--- builds/development/js/controllers/checkins.js
+++ builds/development/js/controllers/checkins.js
@@ -2,5 +2,28 @@ myApp.controller('CheckInsController', function($scope,
   $rootScope, $firebase, $routeParams, 
   $location, Authentication, CountMeetings, FIREBASE_URL) {
 
+  $scope.whichmeeting = $routeParams.mId;
+  $scope.whichuser = $routeParams.uId;
+
+  var ref = new Firebase(FIREBASE_URL + "/users/" +
+    $scope.whichuser + "/meetings/" + 
+    $scope.whichmeeting + '/checkins');
+
+  $scope.addCheckin = function() {
+    var checkinsObj = $firebase(ref);
+
+    var myData = {
+      firstname: $scope.user.firstname,
+      lastname: $scope.user.lastname,
+      email: $scope.user.email,
+      date: Firebase.ServerValue.TIMESTAMP
+    };
+
+    checkinsObj.$push(myData).then(function() {
+      $location.path('/checkins/' + scope.whichuser + '/' +
+        $scope.whichmeeting + '/checkinsList');
+    });//checkinsObj
+  }; //addCheckin
+
 
 }); //CheckInsController
