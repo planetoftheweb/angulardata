371f048850220c19834dbc4afd303b0732c26b5c
Ray Villalobos <ray@lynda.com>
2015-10-05 13:16:45 -0400

    02_03e Modified Meetings Controller

--- builds/angulardata/js/controllers/meetings.js
+++ builds/angulardata/js/controllers/meetings.js
@@ -1,3 +1,25 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
+myApp.controller('MeetingsController', 
+  ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
+  function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
 
-}]);
+    var ref = new Firebase(FIREBASE_URL);
+    var auth = $firebaseAuth(ref);
+
+    auth.$onAuth(function(authUser) {
+      if (authUser) {
+        var meetingsRef = new Firebase(FIREBASE_URL + 'users/' +
+          $rootScope.currentUser.$id + '/meetings');
+        var meetingsInfo = $firebaseArray(meetingsRef);
+        
+
+        $scope.addMeeting = function() {
+          meetingsInfo.$add({
+            name: $scope.meetingname,
+            date: Firebase.ServerValue.TIMESTAMP
+          }).then(function() {
+            $scope.meetingname='';
+          }); //promise
+        }; // addMeeting
+      } // User Authenticated
+    }); // on Auth
+}]); //Controller
