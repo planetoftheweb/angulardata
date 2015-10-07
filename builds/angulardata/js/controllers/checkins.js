0032f2b9b7511a2dabe05911b27467d6d9fd0319
Ray Villalobos <ray@lynda.com>
2015-10-06 20:02:30 -0400

    02_06e Creating a checkin method

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -2,4 +2,24 @@ myApp.controller('CheckInsController',
   ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray','$routeParams', 'FIREBASE_URL',
   function($scope, $rootScope, $firebaseObject, $firebaseArray, $routeParams, FIREBASE_URL) {
 
+    $scope.whichmeeting = $routeParams.mId;
+    $scope.whichuser = $routeParams.uId;
+
+    var ref = new Firebase(FIREBASE_URL + 'users/' +
+      $scope.whichuser + '/meetings/' +
+      $scope.whichmeeting + '/checkins');
+
+    $scope.addCheckin = function() {
+     var checkinsInfo = $firebaseArray(ref);
+     var myData = {
+      firstname: $scope.user.firstname,
+      lastname: $scope.user.lastname,
+      email: $scope.user.email,
+      date: Firebase.ServerValue.TIMESTAMP
+     }; //myData
+
+     checkinsInfo.$add(myData);
+
+    }; //AddCheckin
+
 }]); //Controller    
