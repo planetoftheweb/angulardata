479c031c240e1cf67dcff9e6d1f55ab747fae944
Ray Villalobos <ray@lynda.com>
2015-10-07 16:57:52 -0400

    03_01e Search and Delete

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -13,19 +13,26 @@ myApp.controller('CheckInsController',
     $scope.checkins = checkinsList;
 
     $scope.addCheckin = function() {
-     var checkinsInfo = $firebaseArray(ref);
-     var myData = {
-      firstname: $scope.user.firstname,
-      lastname: $scope.user.lastname,
-      email: $scope.user.email,
-      date: Firebase.ServerValue.TIMESTAMP
-     }; //myData
+      var checkinsInfo = $firebaseArray(ref);
+      var myData = {
+        firstname: $scope.user.firstname,
+        lastname: $scope.user.lastname,
+        email: $scope.user.email,
+        date: Firebase.ServerValue.TIMESTAMP
+      }; //myData
 
-     checkinsInfo.$add(myData).then(function() {
-      $location.path('/checkins/' + $scope.whichuser + '/' +
+      checkinsInfo.$add(myData).then(function() {
+        $location.path('/checkins/' + $scope.whichuser + '/' +
         $scope.whichmeeting + '/checkinsList');
-     }); //Send data to Firebase
-
+      }); //Send data to Firebase
     }; //AddCheckin
+
+    $scope.deleteCheckin = function(id) {
+      var refDel = new Firebase(FIREBASE_URL + 'users/' +
+        $scope.whichuser + '/meetings/' +
+        $scope.whichmeeting + '/checkins/' + id);
+      var record = $firebaseObject(refDel);
+      record.$remove(id);
+    };
 
 }]); //Controller    
