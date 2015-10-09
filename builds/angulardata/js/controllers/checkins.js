8aef594c0ec570d894000f10427ef6c0c2e4b70e
Ray Villalobos <ray@lynda.com>
2015-10-08 22:17:33 -0400

    03_06e Delete Gifts

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -71,4 +71,14 @@ myApp.controller('CheckInsController',
       checkinsArray.$add(myData);
     }; //giveLove
 
+    $scope.deleteLove =function(checkinId, award) {
+      var refLove = new Firebase(FIREBASE_URL + 'users/' +
+        $scope.whichuser + '/meetings/' +
+        $scope.whichmeeting + '/checkins/' + checkinId +
+        '/awards');
+      var record = $firebaseObject(refLove);
+      record.$remove(award);
+    }; //deleteLove
+
+
 }]); //Controller    
