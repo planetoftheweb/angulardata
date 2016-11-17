bb304a1b7a0492a258257d7f2e1dea392c231c48
Ray Villalobos <ray@lynda.com>
2016-11-16 19:50:44 -0500

    Adding Records

--- builds/angulardata/js/controllers/meetings.js
+++ builds/angulardata/js/controllers/meetings.js
@@ -1,3 +1,23 @@
-myApp.controller('MeetingsController', ['$scope', function($scope) {
-  $scope.message = "Success!!!";
-}]);
+myApp.controller('MeetingsController',
+  ['$scope', '$firebaseAuth', '$firebaseArray',
+  function($scope, $firebaseAuth, $firebaseArray) {
+
+    var ref = firebase.database().ref();
+    var auth = $firebaseAuth();
+
+    auth.$onAuthStateChanged(function(authUser) {
+      if(authUser) {
+        var meetingsRef = ref.child('users').child(authUser.uid).child('meetings');
+        var meetingsInfo = $firebaseArray(meetingsRef);
+
+        $scope.addMeeting = function() {
+          meetingsInfo.$add({
+            name: $scope.meetingname,
+            date: firebase.database.ServerValue.TIMESTAMP
+          }).then(function() {
+            $scope.meetingname='';
+          }); //promise
+        } //addMeeting        
+      } //authUser
+    }); //onAuthStateChanged
+}]); //myApp.controller
