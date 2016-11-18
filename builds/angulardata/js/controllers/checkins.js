3cde39dbda5a39865c6a4360d38c3d91783886d2
Ray Villalobos <ray@lynda.com>
2016-11-18 17:59:03 -0500

    03_03e preparing a list of checked in users

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -1,8 +1,8 @@
 myApp.controller('CheckInsController',
-  ['$scope', '$rootScope', '$routeParams', '$firebaseObject', '$firebaseArray',
-  function($scope, $rootScope, $routeParams, $firebaseObject, $firebaseArray) {
+  ['$scope', '$rootScope', '$location', '$routeParams', '$firebaseObject', '$firebaseArray',
+  function($scope, $rootScope, $location, $routeParams, $firebaseObject, $firebaseArray) {
 
-    var ref;
+    var ref, checkinsList;
 
     $scope.whichmeeting = $routeParams.mId;
     $scope.whichuser = $routeParams.uId;
@@ -12,12 +12,18 @@ myApp.controller('CheckInsController',
       .child('meetings').child($scope.whichmeeting)
       .child('checkins');
 
+    checkinsList = $firebaseArray(ref);
+    $scope.checkins = checkinsList;  
+
     $scope.addCheckin = function() {
       $firebaseArray(ref).$add({
         firstname: $scope.user.firstname,
         lastname: $scope.user.lastname,
         email: $scope.user.email,
         date: firebase.database.ServerValue.TIMESTAMP
+      }).then(function() {
+        $location.path('/checkins/' + $scope.whichuser + '/' +
+        $scope.whichmeeting + '/checkinsList')
       }); //$add
     }//addCheckin
 
