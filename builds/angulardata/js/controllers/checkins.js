f0550572ae33ad9c88af94d2c320790acd2ef99d
Ray Villalobos <ray@lynda.com>
2016-11-17 22:14:10 -0500

    03_02e Adding Attendees to our Database

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -1,5 +1,24 @@
 myApp.controller('CheckInsController',
-  ['$scope', '$rootScope', '$firebaseObject', '$firebaseArray',
-  function($scope, $rootScope, $firebaseObject, $firebaseArray) {
+  ['$scope', '$rootScope', '$routeParams', '$firebaseObject', '$firebaseArray',
+  function($scope, $rootScope, $routeParams, $firebaseObject, $firebaseArray) {
+
+    var ref;
+
+    $scope.whichmeeting = $routeParams.mId;
+    $scope.whichuser = $routeParams.uId;
+
+    ref = firebase.database().ref()
+      .child('users').child($scope.whichuser)
+      .child('meetings').child($scope.whichmeeting)
+      .child('checkins');
+
+    $scope.addCheckin = function() {
+      $firebaseArray(ref).$add({
+        firstname: $scope.user.firstname,
+        lastname: $scope.user.lastname,
+        email: $scope.user.email,
+        date: firebase.database.ServerValue.TIMESTAMP
+      }); //$add
+    }//addCheckin
 
 }]); //myApp.controller
