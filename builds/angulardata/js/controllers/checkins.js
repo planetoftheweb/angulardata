0f55ae3fe44ba52cf394357267ff0f19ba7582f3
Ray Villalobos <ray@lynda.com>
2016-11-18 23:01:29 -0500

    04_01e Picking Random Users

--- builds/angulardata/js/controllers/checkins.js
+++ builds/angulardata/js/controllers/checkins.js
@@ -18,6 +18,12 @@ myApp.controller('CheckInsController',
     $scope.order = 'firstname';
     $scope.direction = null;
     $scope.query = '';
+    $scope.recordId='';
+
+    $scope.pickRandom = function() {
+      var whichRecord = Math.round(Math.random() * (checkinsList.length - 1));
+      $scope.recordId = checkinsList.$keyAt(whichRecord);
+    } // pick a random winner
 
     $scope.addCheckin = function() {
       $firebaseArray(ref).$add({
